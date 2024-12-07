9e0b6f06efcd2f69f678dee63c6a6b2daa06c542
Motjiang <motjiangbrilliant@gmail.com>
2024-12-07 14:49:43 +0200

    Added Database context

---
+++ BlogPost_API/Data/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using BlogPost_API.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlogPost_API.Data
+{
+    public class DatabaseContext : IdentityDbContext<ApplicationUser>
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ArticleImage> ArticleImages { get; set; }
+        public DbSet<TokenInfo> TokenInfo { get; set; }
+    }
+}
