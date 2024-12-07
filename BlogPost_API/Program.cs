a5bf2ee48a318795346f69b7d5a2a9183a414296
Motjiang <motjiangbrilliant@gmail.com>
2024-12-07 15:48:48 +0200

    Added identity

--- BlogPost_API/Program.cs
+++ BlogPost_API/Program.cs
@@ -1,4 +1,6 @@
 using BlogPost_API.Data;
+using BlogPost_API.Models.Domain;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +13,13 @@ builder.Services.AddControllers();
 
 builder.Services.AddDbContext<DatabaseContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+// for identity
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<DatabaseContext>()
+    .AddDefaultTokenProviders();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
