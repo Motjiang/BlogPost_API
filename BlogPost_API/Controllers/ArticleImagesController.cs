48d14bccaa33dd7335ba1e0676dcf0c56bef72af
Motjiang <motjiangbrilliant@gmail.com>
2024-12-28 23:01:23 +0200

    Added get all images method

---
+++ BlogPost_API/Controllers/ArticleImagesController.cs
@@ -0,0 +1,43 @@
+﻿using BlogPost_API.Models.DTO;
+using BlogPost_API.Repositories.Interface;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogPost_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticleImagesController : ControllerBase
+    {
+        private readonly IImageRepository _imageRepository;
+
+        public ArticleImagesController(IImageRepository imageRepository)
+        {
+            _imageRepository = imageRepository;
+        }
+
+        //Get all images
+        [HttpGet]
+        public async Task<IActionResult> GetImages()
+        {
+            var images = await _imageRepository.GetAll();
+
+            //convert Domain model to DTO
+            var response = new List<ArticleImageDto>();
+            foreach (var image in images)
+            {
+                response.Add(new ArticleImageDto
+                {
+                    Id = image.Id,
+                    Title = image.Title,
+                    DateCreated = image.DateCreated,
+                    FileExtension = image.FileExtension,
+                    FileName = image.FileName,
+                    Url = image.Url
+                });
+            }
+
+            return Ok(response);
+        }
+    }
+}
